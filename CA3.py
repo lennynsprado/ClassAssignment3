e3d516049f72f2c32d4e0f8f1863e89a9982c739
lennynsprado <lennyn_michaelprado@hotmail.com>
2023-03-01 22:44:25 -0500

    I fixed an issue with the rocket loop. I also created another loopr that counts to 10

--- CA3.py
+++ CA3.py
@@ -1,4 +1,7 @@
 print("This is the third class assignment")
 for i in range(3):
-    print(i)
+    print(i+1)
 print("Rocket has been succesfully launched")
+print("This program also counts to 10")
+for i in range(10):
+    print(i+1)
